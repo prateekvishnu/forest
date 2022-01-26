d2a710cc92630a40aab8be526656dc4c7123b8f2
David Himmelstrup <lemmih@gmail.com>
2022-01-26 17:40:30 +0100

    Improve test correspondence with go.

--- vm/actor/tests/verifreg_remove_datacap_test.rs
+++ vm/actor/tests/verifreg_remove_datacap_test.rs
@@ -10,7 +10,7 @@ use forest_actor::verifreg;
 use forest_actor::verifreg::VerifierParams;
 use forest_actor::{
     account::State, ACCOUNT_ACTOR_CODE_ID, SYSTEM_ACTOR_ADDR, SYSTEM_ACTOR_CODE_ID,
-    VERIFREG_ACTOR_CODE_ID,
+    VERIFREG_ACTOR_CODE_ID, INIT_ACTOR_CODE_ID,
 };
 use std::convert::TryInto;
 use vm::{ExitCode, Serialized};
@@ -23,14 +23,15 @@ fn test_remove_datacap() {
         caller_type: SYSTEM_ACTOR_CODE_ID.clone(),
         ..Default::default()
     };
-    rt.expect_validate_caller_addr(vec![*SYSTEM_ACTOR_ADDR]);
+    // rt.expect_validate_caller_addr(vec![*SYSTEM_ACTOR_ADDR]);
 
     // verifier1, verifier2, verifiedClient := addrs[0], addrs[1], addrs[2]
     // let [verifier1] = create_accounts(&mut rt);
+    // let [verifier1, verifier2] = create_accounts(&mut rt);
     let [verifier1, verifier2, verified_client] = create_accounts(&mut rt);
-    // let verifier_allowance = StoragePower::from(2 * (32 << 30));
+    let verifier_allowance = StoragePower::from(2 * (32u64 << 30));
 
-    // add_verifier(&mut rt, verifier1, verifier_allowance);
+    add_verifier(&mut rt, verifier1, verifier_allowance);
     // addVerifierParams := verifreg.AddVerifierParams{
     // 	Address:   verifier1,
     // 	Allowance: verifierAllowance,
@@ -52,19 +53,38 @@ fn add_verifier(rt: &mut MockRuntime, verifier: Address, allowance: StoragePower
 }
 
 fn create_accounts<const N: usize>(rt: &mut MockRuntime) -> [Address; N] {
+    rt.expect_validate_caller_addr(vec![*SYSTEM_ACTOR_ADDR]);
+    let params = forest_actor::init::ConstructorParams {
+        network_name: "mock".to_string(),
+    };
+    let ret = rt
+        .call(
+            &*INIT_ACTOR_CODE_ID,
+            forest_actor::init::Method::Constructor as vm::MethodNum,
+            &Serialized::serialize(&params).unwrap(),
+        )
+        .unwrap();
+    let mut state_data: forest_actor::init::State = rt.get_state().unwrap();
+
     let mut out = Vec::with_capacity(N);
     let seed: u8 = 0x1F;
     for i in 0..N as u8 {
         let addr = Address::new_bls(&[seed + i; address::BLS_PUB_LEN]).unwrap();
-        rt.call(
-            &*ACCOUNT_ACTOR_CODE_ID,
-            1,
-            &Serialized::serialize(addr).unwrap(),
-        )
-        .unwrap();
+        let id_addr = state_data.map_address_to_new_id(&rt.store, &addr).unwrap();
+        rt.actor_code_cids.insert(id_addr, *ACCOUNT_ACTOR_CODE_ID);
+        // rt.expect_validate_caller_addr(vec![*SYSTEM_ACTOR_ADDR]);
+        // // let x: forest_actor::init::State;
+        // // x.map_address_to_new_id(&rt.store, todo!());
+        // rt.call(
+        //     &*ACCOUNT_ACTOR_CODE_ID,
+        //     forest_actor::account::Method::Constructor as vm::MethodNum,
+        //     &Serialized::serialize(addr).unwrap(),
+        // )
+        // .unwrap();
 
         out.push(addr);
     }
+    rt.replace_state(&state_data);
     out.try_into().unwrap()
 }
 /*
